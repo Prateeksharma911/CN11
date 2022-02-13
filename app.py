15e0810c6b0e2022628256b0cd15ef1901e636b1
Prateeksharma911 <pratek1999sharma@gmail.com>
2022-02-13 16:57:28 +0530

    Added sentry to the project

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, flash, jsonify, render_template, request
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from sqlalchemy import false
 
 app = Flask(__name__)
+
+sentry_sdk.init(
+    dsn="https://3e370496ad9a4efba52d5637fe3aada1@o1141471.ingest.sentry.io/6201702",
+    integrations=[FlaskIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
 
 @app.route('/')
 def hello_world():
@@ -11,5 +25,8 @@ def testpost():
      input_json = request.get_json(force=True) 
      dictToReturn = {'text':input_json['text']}
      return jsonify(dictToReturn)
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=false,host='0.0.0.0')
